1831882c71bfd6f416e7b2074adbbeec46aef6a6
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2017-11-28 20:31:38 +0100

    zadanie 8.6

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var a = 0;
+var b = 0;
+var value = (a * a) - (2 * a * b) + (b *b);
+console.log(value);
+
+if(value>0){
+	console.log("Wynik dodatni")
+}
+else if(value<0){
+	console.log("Wynik ujemny")
+}
+
+else{
+	console.log("Wynik równa się zero")
+}
